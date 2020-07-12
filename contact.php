a95bb8bec2a42b2e12fae4de9cc13e669bdb7c84
humanbeaan <68180747+humanbeaan@users.noreply.github.com>
2020-07-12 18:36:05 +0800

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<meta charset= "utf-8">
+
+		<link rel="stylesheet" type="text/css" href="contact.css">
+	
+    </head>
+
+<body>
+    <div class="contact-title">
+	<h1 align="center"> -Keep In Touch- </h1>
+	<h2> Contact Me!</h2>
+	</div>
+	<main align=center>
+	    <form class="contact-form" method="post" action="contactform.php">
+		    <input name="name" type="text" placeholder="Your Name" ><br>
+			<input name="mail" type="text"  placeholder="Your Email" ><br>
+			<input name="subject" type="text"  placeholder="Subject" ><br>
+			<textarea name="message"  placeholder="Your Message" rows="4" ></textarea><br>
+            <button type="submit" name="submit"> SEND </button>
+        </form>			
+	</main>
+</body>
+_______________________________________________________________________________________________________________________________________
+<footer align=center><br>
+
+						   <div class="hero">
+			<div class="button">
+			    <a href="http://www.instagram.com/eyyahamin_/" class="btn btn-two">Instagram</a>
+				<a href="https://www.youtube.com/channel/UCsik373tWdfigZ7vXBVbpqA" class="btn btn-three">YouTube</a>
+				<a href="http://www.twitter.com/eyyahamin/" class="btn btn-four">Twitter</a><br><br><br>
+</footer>
+</html>
